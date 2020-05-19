5e7bcad89ea25ae544ec73419dc732d0e60e53c2
LaDawson <ladawson@live.co.uk>
2020-05-19 16:36:35 +0000

    initial commit to heroku

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'myCodeDictionary'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_definitions')
+def get_definitions():
+    return render_template("definitions.html",
+                           terms=mongo.db.terms.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
