286b0631eda12664de447d87737abc4d637d8174
LaDawson <ladawson@live.co.uk>
2020-05-21 20:10:21 +0000

    Changed definition pages to be on one render template which iterates over the category id that is selected by the links on the home page

--- app.py
+++ app.py
@@ -11,24 +11,26 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+""" THIS IS THE ROUTE FOR RENDERING THE HOME PAGE
+    WITH THE LIST OF CATEGORIES AND SIGN IN FORM """
+
 
 @app.route('/')
-@app.route('/get_categories')
-def get_categories():
+@app.route('/all_categories')
+def all_categories():
     return render_template("home.html",
                            categories=mongo.db.categories.find())
 
 
-@app.route('/get_htmldefinitions')
-def get_htmldefinitions():
-    return render_template("htmldefinitions.html",
-                           terms=mongo.db.terms.find())
-
-
-@app.route('/get_cssdefinitions')
-def get_cssdefinitions():
-    return render_template("cssdefinitions.html",
-                           terms=mongo.db.terms.find())
+@app.route('/get_category/<category_id>')
+def get_category(category_id):
+    the_cat = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    all_categories = mongo.db.categories.find()
+    the_terms = mongo.db.terms.find()
+    return render_template('category.html',
+                           categories=all_categories,
+                           cat=the_cat,
+                           term=the_terms)
 
 
 @app.route('/add_definition')
