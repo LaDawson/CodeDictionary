da58708778825b699ab3046a157cdaaae5d82953
LaDawson <ladawson@live.co.uk>
2020-05-31 17:32:03 +0000

    created 2 different login routes for the different nav links, also added an admin page to approve the terms from the add page. The category page will not show the term unless it is approved.

--- app.py
+++ app.py
@@ -20,11 +20,8 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/all_categories')
 def all_categories():
-    terms = mongo.db.terms.find()
     return render_template("home.html",
-                           categories=mongo.db.categories.find(),
-                           term=terms,)
-
+                           categories=mongo.db.categories.find())
 
 @app.route('/get_category/<category_id>')
 def get_category(category_id):
@@ -36,7 +33,6 @@ def get_category(category_id):
                             cat=the_cat,
                             term=the_terms)
     
-
 
     """ THIS IS THE ROUTE FOR RENDERING THE ADD
         DEFINITION PAGE AND ALSO THE ROUTE TO INSERT
@@ -80,15 +76,34 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/login_page')
-def login_page():
+@app.route('/login_main')
+def login_main():
     if 'username' in session:
-        return render_template('adddefinition.html')
-    return render_template('login.html')
+        return redirect(url_for('all_categories'))
+    return render_template('mainloginroute.html')
 
 
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/login_mainlogin', methods=['POST'])
+def login_mainlogin():
+    users = mongo.db.users
+    login_user = users.find_one({'user_name': request.form['username']})
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['user_password']) == login_user['user_password']:
+            session['username'] = request.form['username']
+        return redirect(url_for('all_categories'))
+
+    return 'Invalid username/password combination'
+
+
+@app.route('/login_page_addterm')
+def login_page_addterm():
+    if 'username' in session:
+        return redirect(url_for('add_definition'))
+    return render_template('login_addterm.html')
+
+
+@app.route('/login_addterm', methods=['POST'])
+def login_addterm():
     users = mongo.db.users
     login_user = users.find_one({'user_name': request.form['username']})
     if login_user:
@@ -97,6 +112,41 @@ def login():
         return redirect(url_for('add_definition'))
 
     return 'Invalid username/password combination'
+
+
+""" ADMIN PAGE """
+
+
+@app.route('/admin_page')
+def admin_page():
+    the_terms = mongo.db.terms.find()
+    users = mongo.db.users
+    if 'username' in session:
+        the_user = users.find_one({'user_name': session['username']})
+        if the_user['admin'] == "yes":
+            return render_template('adminpage.html',
+                                   terms=the_terms,
+                                   user=the_user)
+    else:
+        return render_template('mainloginroute.html')
+    return redirect(url_for('all_categories'))
+
+
+@app.route('/update_term/<term_id>', methods=["POST"])
+def update_term(term_id):
+    terms = mongo.db.terms
+    terms.update( {'_id': ObjectId(term_id)},
+    {
+        'term_name': "",
+        'category_name': "",
+        'definition': "",
+        'term_use': "",
+        'approved': "yes"
+    })
+    return redirect(url_for('admin_page'))
+
+
+
 
 
 if __name__ == '__main__':
